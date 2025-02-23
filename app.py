a011322700ec6807f4d72e15260fca985cfaf3e9
LEOCORD <dlrkdtlr0504@gmail.com>
2025-02-23 19:51:54 +0900

    QuizGPT
    퀴즈 GPT 만듬

--- app.py
+++ app.py
@@ -1,199 +1,24 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.embeddings import CacheBackedEmbeddings
-from langchain.storage import LocalFileStore
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.memory import ConversationBufferMemory
-from langchain.callbacks.base import BaseCallbackHandler
-import os
 import streamlit as st
-import tempfile
 
 st.set_page_config(
     page_icon="🤖",
-    page_title="DocumentGPT",
+    page_title="FullstackGPT Home",
 )
 
-temp_dir = tempfile.mkdtemp()
+st.title("FullstackGPT Home")
 
-if "openai_api_key" not in st.session_state:
-    st.session_state["openai_api_key"] = None
+st.markdown(
+    """
+    # 안녕하세요!
 
+    저의 FullstackGPT 포트폴리오에 오신것을 환영합니다!
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    제가 만든 앱들입니다😁:
 
-memory = ConversationBufferMemory(
-    return_messages=True,
-    memory_key="history",
+    - [x] [DocumentGPT](/DocumentGPT)   
+    - [ ] [QuizGPT](/QuizGPT)  
+    - [ ] [SiteGPT](/SiteGPT)  
+    - [ ] [MeetingGPT](/MeetingGPT)  
+    - [ ] [InvestorGPT](/InvestorGPT)  
+    """
 )
-
-
-class ChatCallbackHandler(BaseCallbackHandler):
-
-    message = ""
-
-    def on_llm_start(self, *args, **kwargs):
-        self.message_box = st.empty()
-
-    def on_llm_end(self, *args, **kwargs):
-        save_message(self.message, "ai")
-
-    def on_llm_new_token(self, token, *args, **kwargs):
-        self.message += token
-        self.message_box.markdown(self.message)
-
-
-@st.cache_resource(show_spinner="파일 임베딩중...")
-def file_embed_and_retrieve(file):
-
-    file_content = file.read()
-    file_path = os.path.join(temp_dir, file.name)
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=3500,
-        chunk_overlap=500,
-    )
-    loader = UnstructuredFileLoader(file_path)
-    docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.openai_api_key)
-
-    # 수정된 부분: 임시 디렉토리에 벡터 저장
-    vectorstore = FAISS.from_documents(
-        docs,
-        embeddings,
-    )
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
-    return retriever
-
-
-def check_api_key():
-    try:
-        return "OPENAI_API_KEY" in st.secrets
-    except FileNotFoundError:
-        return False
-
-
-def get_api_key(key):
-    st.session_state["openai_api_key"] = key
-
-
-def save_message(message, role):
-    st.session_state["messages"].append({"message": message, "role": role})
-
-
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.markdown(message)
-    if save:
-        save_message(message, role)
-
-
-def paint_history():
-    for message in st.session_state["messages"]:
-        send_message(message["message"], message["role"], save=False)
-
-
-def load_memory(_):
-    return memory.load_memory_variables({})["history"]
-
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            Answer the question using ONLY the following context.
-            If you don't know the answer just say you don't know.
-            DON'T make anything up.
-            And if the user asks a question in Korean, answer in Korean, and if the user asks a question in English, answer in English.
-     
-            Context: {context}
-            """,
-        ),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{question}"),
-    ]
-)
-
-
-def invoke_chain(question):
-    result = chain.invoke(question)
-
-    memory.save_context({"input": question}, {"output": result.content})
-    return result
-
-
-with st.sidebar:
-    if not st.session_state.openai_api_key:
-        st.markdown(
-            """
-        ### OpenAI API 키 입력
-        1. https://platform.openai.com/account/api-keys 에서 API 키를 발급받으세요.
-        2. 발급받은 API 키를 아래에 입력하세요.
-        3. API 키는 세션에만 저장되며, 브라우저를 닫으면 사라집니다.
-        """
-        )
-        api_key = st.text_input("OpenAI API 키를 입력하세요", type="password")
-        if api_key:
-            st.session_state.openai_api_key = api_key
-            st.success("API 키가 입력되었습니다!")
-            st.rerun()
-        elif not st.session_state.get("openai_api_key"):
-            st.warning("API 키를 입력해주세요!")
-    else:
-        file = st.file_uploader(
-            "TXT, PDF, DOCX 파일을 업로드 하세요.",
-            type=["txt", "pdf", "docx"],
-        )
-
-if st.session_state.openai_api_key:
-    llm = ChatOpenAI(
-        temperature=0.1,
-        streaming=True,
-        openai_api_key=st.session_state.openai_api_key,
-        callbacks=[
-            ChatCallbackHandler(),
-        ],
-    )
-
-    st.title("DocumentGPT")
-
-    st.markdown(
-        """
-        안녕하세요!
-
-        챗봇을 사용하여 파일에 대해 인공지능에게 질문하세요.
-
-        파일을 사이드바에 업로드해 주세요.
-        """
-    )
-
-    if file:
-        retriever = file_embed_and_retrieve(file)
-        send_message("파일을 읽었습니다. 무엇을 도와드릴까요?", "ai", save=False)
-        paint_history()
-        message = st.chat_input("파일에 대해 무엇이든지 물어보세요")
-        if message:
-            send_message(message, "human")
-            chain = (
-                {
-                    "context": retriever,
-                    "question": RunnablePassthrough(),
-                    "history": load_memory,
-                }
-                | prompt
-                | llm
-            )
-
-            with st.chat_message("ai"):
-                invoke_chain(message)
-    else:
-        st.session_state["messages"] = []
---
+++ pages/02_QuizGPT.py
@@ -0,0 +1,213 @@
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.retrievers import WikipediaRetriever
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+
+import os
+import tempfile
+import json
+import streamlit as st
+
+st.set_page_config(
+    page_icon="🧐",
+    page_title="QuizGPT",
+)
+
+st.title("퀴즈 GPT")
+
+temp_dir = tempfile.mkdtemp()
+
+
+if "openai_api_key" not in st.session_state:
+    st.session_state["openai_api_key"] = None
+
+
+function = {
+    "name": "create_quiz",
+    "description": "The function reads the documentation and returns a quiz with a list of questions and answers.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "questions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "type": "string",
+                        },
+                        "answers": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "answer": {
+                                        "type": "string",
+                                    },
+                                    "correct": {
+                                        "type": "boolean",
+                                    },
+                                },
+                                "required": ["answer", "correct"],
+                            },
+                        },
+                    },
+                    "required": ["question", "answers"],
+                },
+            }
+        },
+        "required": ["questions"],
+    },
+}
+
+llm = ChatOpenAI(
+    temperature=0.1,
+).bind(
+    function_call={
+        "name": "create_quiz",
+    },
+    functions=[
+        function,
+    ],
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Create 10 questions in KOREAN to test your knowledge of the text based ONLY on the following context.
+
+    Also creates questions by determining the difficulty based on the value assigned to "Level" below.
+
+    A Level value of “Hard” will make the question very challenging, while “Easy” will make the question easy enough for anyone with common sense.
+
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+    Your turn!
+
+    Context: {context}
+
+    Level: {level}
+""",
+        ),
+    ]
+)
+
+chain = prompt | llm
+
+
+@st.cache_resource(show_spinner="파일 읽는중...")
+def file_embed_and_retrieve(file):
+
+    file_content = file.read()
+    file_path = os.path.join(temp_dir, file.name)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=3500,
+        chunk_overlap=500,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+@st.cache_resource(show_spinner="위키피디아 검색중...")
+def wiki_search(topic):
+    retriever = WikipediaRetriever(top_k_results=1, lang="ko")
+    docs = retriever.get_relevant_documents(topic)
+    return docs
+
+
+@st.cache_resource(show_spinner="퀴즈 생성중...")
+def invoke_and_create_quiz(_docs, topic, level):
+    quiz = chain.invoke(
+        {
+            "context": _docs,
+            "level": level,
+        }
+    ).additional_kwargs[
+        "function_call"
+    ]["arguments"]
+    return json.loads(quiz)
+
+
+with st.sidebar:
+    docs = None
+    if not st.session_state.openai_api_key:
+        st.markdown(
+            """
+        ### OpenAI API 키 입력
+        1. https://platform.openai.com/account/api-keys 에서 API 키를 발급받으세요.
+        2. 발급받은 API 키를 아래에 입력하세요.
+        3. API 키는 세션에만 저장되며, 브라우저를 닫으면 사라집니다.
+        """
+        )
+        api_key = st.text_input("OpenAI API 키를 입력하세요", type="password")
+        if api_key:
+            st.session_state.openai_api_key = api_key
+            st.success("API 키가 입력되었습니다!")
+            st.rerun()
+        elif not st.session_state.get("openai_api_key"):
+            st.warning("API 키를 입력해주세요!")
+    else:
+        value = st.selectbox("선택하세요", ["파일", "위키피디아"])
+        level = st.selectbox("난이도를 선택하세요", ["Easy", "Hard"])
+        if value == "파일":
+            file = st.file_uploader(
+                "TXT, PDF, DOCX 파일을 업로드 하세요.",
+                type=["txt", "pdf", "docx"],
+            )
+            answer_toggle = st.toggle("정답보기", value=False)
+            if file:
+                docs = file_embed_and_retrieve(file)
+                topic = file.name
+        else:
+            topic = st.text_input("위키피디아에서 검색할 키워드를 입력하세요")
+            answer_toggle = st.toggle("정답보기", value=False)
+            if topic:
+                docs = wiki_search(topic)
+
+if not docs:
+    st.markdown(
+        """
+    QuizGPT에 오신것을 환영합니다!
+
+    여러분이 요청한 위키피디아 기사나 업로드한 파일로 퀴즈를 만들어 지식을 테스트하고 공부하는 데 도움을 드립니다.
+
+    사이드바에서 파일을 업로드하거나 위키백과에서 검색하여 시작하세요.
+    """
+    )
+else:
+    quiz_object = invoke_and_create_quiz(docs, topic, level)
+    with st.form("question form"):
+        st.session_state["count"] = 0
+        for question in quiz_object["questions"]:
+            value = st.radio(
+                question["question"],
+                [answer["answer"] for answer in question["answers"]],
+                index=None,
+            )
+            if answer_toggle:
+                for elem in question["answers"]:
+                    if elem["correct"] == True:
+                        st.info(f"💡 정답:  {elem['answer']}")
+                        break
+
+            if value:
+                if {"answer": value, "correct": True} in question["answers"]:
+                    st.success("정답입니다.")
+                    st.session_state["count"] += 1
+                else:
+                    st.error("틀렸습니다.")
+        st.form_submit_button("제출")
+    st.write(f"점수: {st.session_state['count']} / {len(quiz_object['questions'])}")
+    if st.session_state["count"] == len(quiz_object["questions"]):
+        st.balloons()
